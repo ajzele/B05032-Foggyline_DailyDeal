1485c09098b711aa3b9547d53d327152e736cdb9
Branko Ajzele <ajzelegmail.com>
2015-08-31 12:04:13 +0200

    Book related module.

---
+++ Model/Resource/Indexer/Price.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Foggyline\DailyDeal\Model\Resource\Indexer;
+
+class Price extends \Magento\Catalog\Model\Resource\Product\Indexer\Price\DefaultPrice
+{
+
+}
---
+++ Model/Resource/Indexer/Stock.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Foggyline\DailyDeal\Model\Resource\Indexer;
+
+class Stock extends \Magento\CatalogInventory\Model\Resource\Indexer\Stock\DefaultStock
+{
+
+}
